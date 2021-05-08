75bb23a54c0131ac95d1dce3e612fa51bc9e4bf5
khattab88 <m_khatab_88@hotmail.com>
2021-05-08 10:48:53 +0200

    Implement Seed() to populate tith test data (COMMENTED)

--- Repositories/Migrations/Configuration.cs
+++ Repositories/Migrations/Configuration.cs
@@ -1,6 +1,8 @@
 ﻿namespace Repositories.Migrations
 {
+    using Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -14,10 +16,60 @@
 
         protected override void Seed(Repositories.ExpressoDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+           
+            //var countries = new List<Country>
+            //{
+            //    new Country 
+            //    { 
+            //        Name = "Egypt", Slug = "egypt", Alias = "EGY", Currency = "EGP",
+            //        Cities = new List<City> 
+            //        {
+            //            new City 
+            //            {
+            //                Name = "Cairo", Slug = "cairo",
+            //                Areas = new List<Area> 
+            //                {
+            //                    new Area { Name = "Heliopolis", Slug = "heliopolis" },
+            //                    new Area { Name = "Maadi", Slug = "maadi" },
+            //                    new Area { Name = "DownTown", Slug = "downtown" },
+            //                    new Area { Name = "Nasr City", Slug = "nasr-city" },
+            //                }
+            //            },
+            //            new City 
+            //            { 
+            //                Name = "Giza", Slug = "giza",
+            //                Areas = new List<Area> 
+            //                {
+            //                    new Area { Name = "Mohandesen", Slug = "mohandesen" },
+            //                    new Area { Name = "Haram", Slug = "haram" },
+            //                    new Area { Name = "Dokki", Slug = "dokki" },
+            //                    new Area { Name = "6th October", Slug = "6th-october" },
+            //                }
+            //            },
+            //        }
+            //    },
+            //    new Country { Name = "UAE", Slug = "uae", Alias = "uae", Currency = "AED" },
+            //    new Country { Name = "Saudi Arabia", Slug = "saudi-arabia", Alias = "SAUDI", Currency = "SAR" },
+            //};
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
+
+            //foreach (var country in countries)
+            //{
+            //    context.Countries.AddOrUpdate(country);
+
+            //    foreach (var city in country.Cities)
+            //    {
+            //        // city.CountryId = country.Id;
+            //        context.Cities.AddOrUpdate(city);
+
+            //        foreach (var area in city.Areas)
+            //        {
+            //            // area.CityId = city.Id;
+            //            context.Areas.AddOrUpdate(area);
+            //        }
+            //    }
+            //}
+
         }
     }
 }
