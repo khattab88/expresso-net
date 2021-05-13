4404d52fd8a87a7c1df9738e19175506cd99ceb4
khattab88 <m_khatab_88@hotmail.com>
2021-05-13 21:09:25 +0200

    remove unnecessary action methods in home controller

--- CMS/Controllers/HomeController.cs
+++ CMS/Controllers/HomeController.cs
@@ -28,19 +28,6 @@ namespace CMS.Controllers
             return View(tags);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
 
         protected override void Dispose(bool disposing)
         {
