3763c69c4ea9eebc254d3c52bedafd7d8cefbace
khattab88 <m_khatab_88@hotmail.com>
2021-05-17 18:57:53 +0200

    Implement Create and Edit actions (Countries)

---
+++ CMS/Controllers/CountriesController.cs
@@ -0,0 +1,42 @@
+﻿using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CMS.Controllers
+{
+    public class CountriesController : Controller
+    {
+        private readonly ExpressoDbContext _context;
+
+        public CountriesController()
+        {
+            _context = new ExpressoDbContext();
+        }
+
+        // GET: Countries
+        public ActionResult Index()
+        {
+            var countries = _context.Countries.ToList();
+
+            return View(countries);
+        }
+
+        public ActionResult New() 
+        {
+            return View();
+        }
+
+        public ActionResult Edit(string id) 
+        {
+            var country = _context.Countries.SingleOrDefault(c => c.Id.ToString() == id);
+
+            if (country == null)
+                return HttpNotFound();
+
+            return View(country);
+        }
+    }
+}
