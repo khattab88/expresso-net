4b23b5de8ac0a4ccd630dedffebb3ee0c7cc0d26
khattab88 <m_khatab_88@hotmail.com>
2021-05-22 03:15:34 +0200

    implement delete tag

---
+++ CMS/Controllers/Api/TagsController.cs
@@ -0,0 +1,50 @@
+﻿using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace CMS.Controllers.Api
+{
+    public class TagsController : ApiController
+    {
+        private readonly ExpressoDbContext _context;
+
+        public TagsController()
+        {
+            _context = new ExpressoDbContext();
+        }
+
+        [HttpDelete]
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Delete(string id)
+        {
+            try
+            {
+                var existingTag = _context.Tags.SingleOrDefault(t => t.Id.ToString() == id);
+
+                if (existingTag == null)
+                    return NotFound();
+
+                _context.Tags.Remove(existingTag);
+                _context.SaveChanges();
+
+                return StatusCode(HttpStatusCode.NoContent);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+
+            base.Dispose(disposing);
+        }
+    }
+}
